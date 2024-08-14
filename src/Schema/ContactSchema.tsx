c8b42e17f79f6c20bdf21eae014844572810d8e6
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-08-13 21:49:46 -0300

    Adicionando regex a uma constante

--- src/Schema/ContactSchema.tsx
+++ src/Schema/ContactSchema.tsx
@@ -1,9 +1,10 @@
 import { z } from "zod"
 
 const phoneRegex: RegExp = /^\(\d{2}\) \d{5}-\d{4}$/
+const nameRegex: RegExp = /^[^\d]*$/
 
 export const contactSchema = z.object({
-  name: z.string().regex(/^[^\d]*$/, {message: "o nome não pode conter números."}).max(32, {message: "deve ser menos que 33 caracteres."}),
+  name: z.string().regex(nameRegex, {message: "o nome não pode conter números."}).max(32, {message: "deve ser menos que 33 caracteres."}),
   phone: z.string().regex(phoneRegex, { message: "deve ser (XX)XXXXX-XXXX." }),
   email: z.string().email({message: "E-mail inválido."}).max(60, {message: "deve ser menos que 61 caracteres."}),
   address: z.string().max(255, {message: "deve ser menos que 256 caracteres."}),
